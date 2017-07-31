59b056d46ff8076c2376d77a70696cbb30ea9de4
Matias Camiletti <matias.camiletti@gmail.com>
2017-07-31 17:30:27 -0300

    Agregado metodos para obtener el Token y Name del dispositivo.

--- core/src/main/java/com/mobileia/core/Mobileia.java
+++ core/src/main/java/com/mobileia/core/Mobileia.java
@@ -1,5 +1,7 @@
 package com.mobileia.core;
 
+import android.os.Build;
+
 /**
  * Created by matiascamiletti on 28/7/17.
  */
@@ -13,6 +15,10 @@ public class Mobileia {
      * Almacena el APP_ID
      */
     protected int mAppId = 0;
+    /**
+     * Almacena el Token ID del dispositivo
+     */
+    protected String mDeviceToken = "";
 
     /**
      * Obtiene la instancia creada
@@ -39,5 +45,33 @@ public class Mobileia {
      */
     public int getAppId(){
         return this.mAppId;
+    }
+
+    /**
+     * Configura el device token
+     * @param token
+     */
+    public void setDeviceToken(String token){
+        this.mDeviceToken = token;
+    }
+
+    /**
+     * Obtiene el token ID del dispositivo
+     * @return
+     */
+    public String getDeviceToken(){ return this.mDeviceToken; }
+
+    /**
+     * Funcion que devuelve el Nombre del dispositivo, con la marca y el modelo
+     * @return
+     */
+    public String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return model;
+        } else {
+            return manufacturer + " " + model;
+        }
     }
 }
