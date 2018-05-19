8290d2588c14d3b3432f8ffa2a7a2cd3df32ac90
Matias Camiletti <matias.camiletti@gmail.com>
2018-05-19 14:07:17 -0300

    Agregado Callback para Retrofit que reduce el codigo de las peticiones.

---
+++ core/src/main/java/com/mobileia/core/rest/RestBodyCallback.java
@@ -0,0 +1,34 @@
+package com.mobileia.core.rest;
+
+import com.mobileia.core.entity.Error;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+abstract public class RestBodyCallback<T> implements Callback<RestBody<T>> {
+
+    @Override
+    public void onResponse(Call<RestBody<T>> call, Response<RestBody<T>> response) {
+        // Verificar si la respuesta fue incorrecta
+        if (!response.isSuccessful() || !response.body().success) {
+            // Verificar si hubo un error y no se pudo obtener respuesta correcta del servidor
+            if(response.body() == null){
+                onError(new Error(-2, "Error Inesperado!"));
+            }else{
+                onError(response.body().error);
+            }
+            return;
+        }
+        // Enviamos que se obtuvo respuesta correctamente
+        onSuccess(response.body().response);
+    }
+
+    @Override
+    public void onFailure(Call<RestBody<T>> call, Throwable t) {
+
+    }
+
+    abstract public void onSuccess(T body);
+    abstract public void onError(Error error);
+}
